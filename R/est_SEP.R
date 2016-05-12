52210dff3c3e685165cb07296c04e9271c62e934
SimoneHermann <hermann@statistik.tu-dortmund.de>
2016-05-12 10:39:00 +0200

    prediction of SEP added

--- R/est_SEP.R
+++ R/est_SEP.R
@@ -10,7 +10,7 @@
 #' @param priorRatio function(old, new) for prior ratio, if missing => noninformative
 #' @param proposals list of functions: draw(old) and ratio(new, old)
 #' @param Lmax maximal number of tension wires that can break
-#'
+#' @param propSd proposal standard deviation
 #' @return p x n dimensional matrix of posterior samples, p length of vector xi
 #' @examples
 #' wt <- rexp(20, exp(- 1 + 0.25*200/(35-(0:19))))
@@ -31,9 +31,9 @@
 #' wt <- rexp(20, exp(- 1 + 0.25*s[a]/(35-(0:19))))
 #' cumsum(wt)
 #' })
-#' chain <- est_SEP(jumpTimes, start = c(1, 0.25), s = s)
+#' chain <- est_SEP(jumpTimes, start = c(1, 0.25), s = s, n = 1000)
 #' @export
-est_SEP <- function(jumpTimes, Tend, start, n = 5000, s = 200, f_xi, priorRatio, proposals, Lmax = 35){
+est_SEP <- function(jumpTimes, Tend, start, n = 5000, s = 200, f_xi, priorRatio, proposals, Lmax = 35, propSd){
 
   if(is.list(jumpTimes)){
     N <- length(jumpTimes)
@@ -75,34 +75,26 @@ est_SEP <- function(jumpTimes, Tend, start, n = 5000, s = 200, f_xi, priorRatio,
     priorRatio <- function(xi_drawn, xi_old) 1
   }
 
-
+  if(missing(propSd)) propSd <- abs(start)/10 + 0.001
   if(missing(proposals)){
-    proposal <- function(parOld, propSd){
-      help <- rnorm(length(parOld), sqrt(parOld), propSd)
-      help^2
-    }
-    proposalRatio <- function(parOld, parNew){
-      prod(sqrt(parNew)/sqrt(parOld))
-    }
-
     proposals <- list()
-    proposals$draw <- function(xi_old){
-      proposal(xi_old, (abs(start)+0.1)/10)
+    proposals$draw <- function(xi_old, propSd){
+      proposal(xi_old, propSd)
     }
-    proposals$ratio <- function(xi_drawn, xi_old){
-      proposalRatio(xi_old, xi_drawn)
+    proposals$ratio <- function(xi_drawn, xi_old, propSd){
+      proposalRatio(xi_old, xi_drawn, propSd)
     }
   }
 
   xi_old <- start
-  xi_out <- matrix(0, length(start), n)
+  xi_out <- matrix(0, n, length(start))
   LikOld <- Lik(xi_old)
 
   for(count in 1:n){
-    xi_drawn <- proposals$draw(xi_old)
+    xi_drawn <- proposals$draw(xi_old, propSd)
 
     LikNew <- Lik(xi_drawn)
-    ratio <- proposals$ratio(xi_drawn, xi_old)
+    ratio <- proposals$ratio(xi_drawn, xi_old, propSd)
     ratio <- ratio*priorRatio(xi_drawn, xi_old)
     ratio <- ratio*LikNew/LikOld
     if(is.na(ratio)) ratio <- 0
@@ -111,10 +103,84 @@ est_SEP <- function(jumpTimes, Tend, start, n = 5000, s = 200, f_xi, priorRatio,
       xi_old <- xi_drawn
       LikOld <- LikNew
     }
-    xi_out[,count] <- xi_old
+    xi_out[count,] <- xi_old
   }
 
 
   xi_out
 }
+
+#' Prediction of SEP
+#'
+#' @description Bayesian prediction of the self-exciting process.
+#' @param xi Markov chains
+#' @param s stress
+#' @param J index
+#' @param f_xi_mat matrix-vise function
+#' @param len length of samples
+#' @param end how many jumps
+#' @param grid fineness degree
+#' @param Lmax maximal number of tension wires that can break
+#' 
+#' @examples
+#' s <- seq(100, 300, length = 10)
+#' f_xi <- function(y, xi) xi[1] - xi[2]*y
+#' jumpTimes <- lapply(1:10, function(a){
+#' wt <- rexp(20, exp(- f_xi(s[a]/(35-(0:19)), c(1, 0.25))))
+#' cumsum(wt)
+#' })
+#' \dontrun{
+#' chain <- est_SEP(jumpTimes, start = c(1, 0.25), s = s, n = 11000)
+#' pred <- pred_SEP(chain[seq(1001, 11000, by = 10),], s = s[1], J = 1, f_xi_mat = function(y, xi) xi[,1] - xi[,2]*y)
+#' qu <- apply(pred, 2, quantile, c(0.025, 0.975))
+#' plot(jumpTimes[[1]], ylim = range(c(jumpTimes, range(qu))))
+#' lines(qu[1,])
+#' lines(qu[2,])
+#' }
+#' @export
+
+pred_SEP <- function(xi, s, J = 1, f_xi_mat, len = 100, end = 20, grid = 0.01, Lmax = 35){
+
+  lambda <- function(j){
+    exp(-f_xi_mat(s/(Lmax-j-1), xi)) 
+  }
+  Fun <- function(cand, j){
+    1-mean(exp(-lambda(j)*cand))
+  }
+  
+  drawT <- function(j){
+    u <- runif(1)
+    cand <- 1
+    memory <- cand
+    
+    while(length(unique(memory)) == length(memory)){
+      if(Fun(cand, j) < u){
+        cand <- cand*2
+        memory <- c(memory,cand)
+      }else{
+        cand <- cand/2
+        memory <- c(memory,cand)
+      }
+    }  
+    lower <- memory[length(memory)]
+    upper <- memory[length(memory)-1]
+    diff <- upper - lower
+    while(diff >= grid){
+      if(Fun(lower+diff/2,j) < u){
+        lower <- lower+diff/2
+      }else{
+        upper <- lower+diff/2
+      }
+      diff <- upper - lower
+    }  
+    (lower+upper)/2
+  }
+  
+  result <- sapply(J:end, function(k){
+    replicate(len, drawT(k))
+  })
+  t(apply(result, 1, cumsum))
+}
+
+
 
